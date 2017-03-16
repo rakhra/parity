297fbec9da61b8fb005f474ad35b2824b9a48635
Sandeep Rakhra <sandeep@bluefields.com>
2017-03-16 17:53:22 +0800

    Add session store.

--- app.js
+++ app.js
@@ -27,7 +27,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({secret: 'secret', resave: false, saveUninitialized : false}));
+
+app.use(session({
+  secret: 'secret', 
+  resave: false, 
+  saveUninitialized : false,
+  store: new MongoStore({mongooseConnection: mongoose.connection}),
+  cookie: {maxAge: 180 * 60 * 1000}}));
+
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -35,6 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 });
 
@@ -68,3 +76,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
